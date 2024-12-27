dfddeb04781c1a78fdf94ac3f3ddfc87414a4f43
amitchell94 <andymitchell1@live.co.uk>
2024-12-27 17:31:06 -0600

    basic message sending

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,29 @@ import './App.css'
 
 function App() {
   const [message, setMessage] = useState("")
+  const [messageList, setMessageList] = useState<string[]>([])
+
+  const sendMessage = (event: React.FormEvent) => {
+    event.preventDefault();
+    setMessageList([...messageList, message]);
+    setMessage("");
+  }
 
   return (
     <>
+    <ol>
+      {messageList?.map((msg, i) => (
+        <li key={i}>{msg}</li>
+      ))}
+    </ol>
+    <form onSubmit={sendMessage}>
       <input
           type="text"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
+      <button type="submit">Send</button>
+    </form>
     </>
   )
 }
